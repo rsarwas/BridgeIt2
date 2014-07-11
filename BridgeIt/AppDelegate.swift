f347b6f6a4c10c264ce1b9510b72f9a8237e1f24
Regan Sarwas @ Bob <rsarwas@gmail.com>
2014-07-10 18:31:53 -0800

    Initial Commit

---
+++ BridgeIt/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  BridgeIt
+//
+//  Created by Regan Sarwas on 2014-07-10.
+//  Copyright (c) 2014 Regan Sarwas. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ BridgeIt/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  BridgeIt
+//
+//  Created by Regan Sarwas on 2014-07-10.
+//  Copyright (c) 2014 Regan Sarwas. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
